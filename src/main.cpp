6ac9f5e852f6d6f72641710614add32c13353227
Sorre <angrepenguin@gmail.com>
2024-12-25 01:49:40 -0500

    implemented libcurl to retrieve card set information from scryfall implemented conversion from json to struct

--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,101 @@
 #include <SQLiteCpp/SQLiteCpp.h>
 #include <spdlog/spdlog.h>
 #include <wolfssl/openssl/ssl.h>
+#include <nlohmann/json.hpp>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <ctime>
 
+struct Set {
+	std::string uuid; // unique ID
+	std::string setCode;
+	std::string mtgoCode;
+	std::string arenaCode;
+	uint16_t tcgplayerId;
+	std::string name;
+	std::string type;
+	std::time_t releaseDate;
+	std::string blockCode;
+	std::string block;
+	std::string parentCode;
+	uint16_t cardCount;
+	uint16_t printedSize;
+	bool digital;
+	bool foilOnly;
+	bool nonfoilOnly;
+	std::string IconURI;
+};
+
+using json = nlohmann::json;
+
+std::string downloadData(const std::string& dlURL);
+size_t writeFunction(void *ptr, size_t size, size_t nmemb, std::string* data);
+
+Set jsonToSet(json& set);
+time_t stringToTime(json& date);
 
 int main(int argc, char** argv) {
-	CURL *curl = curl_easy_init();
-	CURLcode imgresult;
-	FILE *fp;
-	if (curl) {
-		errno_t err = fopen_s(&fp, "./google.jpg", "wb");
-		curl_easy_setopt(curl, CURLOPT_URL, "https://images.squarespace-cdn.com/content/v1/539f49a3e4b0fc234463a04d/1489013554760-TXQZT540SWRPDVOW3UMM/image-asset.jpeg?format=2500w");
-    	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, NULL);
-    	curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
-		imgresult = curl_easy_perform(curl);
+	json j;
+	std::vector<Set> sets;
+	std::string rawJSON = downloadData("https://api.scryfall.com/sets");
+	j = json::parse(rawJSON, nullptr, false);
+	for (const auto& i : j["data"].items()) {
+		sets.emplace_back(jsonToSet(i.value()));
 	}
-	curl_easy_cleanup(curl);
-	fclose(fp); 
+	std::cout << "\n" << sets.size();
 	return EXIT_SUCCESS;
 }
 
-//https://api.scryfall.com/cards/!set:uuid!?format=image&face=!prop:side!
-//https://api.scryfall.com/cards/multiverse/!set:muid!?format=image
+Set jsonToSet(json& set) {
+	Set s;
+	s.uuid = set["id"];
+	s.setCode = set["code"];
+	s.mtgoCode = set.value("mtgo_code", "");
+	s.arenaCode = set.value("arena_code", "");
+	s.tcgplayerId = set.value("tcgplayer_id", 0);
+	s.name = set["name"];
+	s.type = set["set_type"];
+	s.releaseDate = stringToTime(set["released_at"]);
+	s.blockCode = set.value("block_code", "");
+	s.block = set.value("block", "");
+	s.parentCode = set.value("parent_set_code", "");
+	s.cardCount = set["card_count"];
+	s.printedSize = set.value("printed_size", 0);
+	s.digital = set["digital"];
+	s.foilOnly = set["foil_only"];
+	s.nonfoilOnly = set["nonfoil_only"];
+	s.IconURI = set["icon_svg_uri"];
+	return s;
+}
+
+time_t stringToTime(json& date) {
+	std::string dateStr = date;
+	std::istringstream dateStream(dateStr);
+	tm tm = {};
+	dateStream >> std::get_time(&tm, "%Y-%m-%d");
+	return mktime(&tm);
+}
+
+size_t writeFunction(void *ptr, size_t size, size_t nmemb, std::string* data) {
+    data->append((char*) ptr, size * nmemb);
+    return size * nmemb;
+}
+
+std::string downloadData(const std::string& dlURL) {
+	CURL *curl = curl_easy_init();
+	CURLcode dlResult;
+	std::string data;
+	if (curl) {
+		curl_easy_setopt(curl, CURLOPT_URL, dlURL.c_str());
+		curl_easy_setopt(curl, CURLOPT_USERAGENT, "birdseye/0.1");
+    	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeFunction);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &data);
+		dlResult = curl_easy_perform(curl);
+	}
+	curl_easy_cleanup(curl);
+	return data;
+}
+
 //https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=!set:muid!&type=card
 //https://gatherer.wizards.com/Handlers/Image.ashx?name=!name!&type=card
--- xmake.lua
+++ xmake.lua
@@ -5,7 +5,7 @@ if #os.dirs("lib/*") > 0 then
     includes("lib/**/xmake.lua")
 end
 
-local sharedPackages = { "spdlog", "sqlitecpp", "libcurl", "wolfssl" }
+local sharedPackages = { "spdlog", "sqlitecpp", "libcurl", "wolfssl", "nlohmann_json" }
 local releasePackages = {}
 local debugPackages = {}
 
